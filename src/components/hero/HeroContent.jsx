7d058eda897f0f40c609b14c9c1b88204a26d65d
VCarames <vcarames-dev@outlook.com>
2025-05-10 19:24:40 -0700

    "Hero Content" Update

--- src/components/hero/HeroContent.jsx
+++ src/components/hero/HeroContent.jsx
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 function HeroContent() {
   return (
     <div className="hero__content">
-      <p className="hero__text">
-        The arts in the collection of the Modern Art Gallery all started from a
-        spark of inspiration. Will these pieces inspire you? Visit us and find
-        out.
-      </p>
+      <h1 className="hero__heading">Modern Art Gallery</h1>
+      <div className="hero__info">
+        <p className="hero__text">
+          The arts in the collection of the Modern Art Gallery all started from
+          a spark of inspiration. Will these pieces inspire you? Visit us and
+          find out.
+        </p>
 
-      <Link to="location" className="hero__btn button">
-        <span className="button__text">Our location</span>
-        <span className="button__icon">
-          <img src="./assets/icon-arrow-right.svg" alt="" />
-        </span>
-      </Link>
+        <Link to="location" className="hero__btn button">
+          <span className="button__text">Our location</span>
+          <span className="button__icon">
+            <img src="src/assets/icons/icon-arrow-right.svg" alt="" />
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
